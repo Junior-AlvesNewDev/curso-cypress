2273ce2f996364b4a97e9866b046e50081cd48ef
Junior-AlvesNewDev <onoffjunior@gmail.com>
2025-12-01 20:39:14 -0300

    inserindo conta com mesmo nome esperando code 400

--- cypress/cypress/e2e/barriga/functional.cy.js
+++ cypress/cypress/e2e/barriga/functional.cy.js
@@ -1,19 +1,19 @@
 /// <reference types="cypress" />
 
 import loc from "../../support/locators"
+import '../../../support/commandsContas'
+
 
 describe('Teste funcional', () => {
     beforeEach(() => {
         cy.login('onoffjunior@gmail.com', '#@Barriga')
-        //cy.resetApp()
+        cy.resetApp()
     })
 
 
     it('Inserir conta', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Internet webs')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()       
+        cy.acessarMenuConta()
+        cy.inserirConta('Internet webs')              
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso!')
     })
 
@@ -28,6 +28,13 @@ describe('Teste funcional', () => {
 
         cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
+    })
+
+    it('criar conta com o mesmo nome', () => {
+        cy.acessarMenuConta()
+        cy.get(loc.CONTAS.NOME).type('Conta alterada')
+        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'Code 400')
     })
 
     it('Excluir conta', () => {
