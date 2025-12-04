c4d60a99d2d85d98c979072148447b34983cd8bd
Junior-AlvesNewDev <onoffjunior@gmail.com>
2025-12-04 16:56:40 -0300

    inserindo movimentos e contas duplicadas

--- cypress/cypress/e2e/barriga/functional.cy.js
+++ cypress/cypress/e2e/barriga/functional.cy.js
@@ -7,7 +7,7 @@ import '../../../support/commandsContas'
 describe('Teste funcional', () => {
     beforeEach(() => {
         cy.login('onoffjunior@gmail.com', '#@Barriga')
-        cy.resetApp()
+        //cy.resetApp()
     })
 
 
@@ -32,9 +32,23 @@ describe('Teste funcional', () => {
 
     it('criar conta com o mesmo nome', () => {
         cy.acessarMenuConta()
-        cy.get(loc.CONTAS.NOME).type('Conta alterada')
+        cy.get(loc.CONTAS.NOME).type('Conta para saldo')
         cy.get(loc.CONTAS.BTN_SALVAR).click()
-        cy.get(loc.MESSAGE).should('contain', 'Code 400')
+        cy.get(loc.MESSAGE).should('contain', 'Erro: Error: Request failed with status code 400')
+    })
+
+    it('Salvando Movimentos', () => {
+        cy.get(loc.MENU.MOVIMENTACAO).click();
+
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        cy.get(loc.EXTRATO.LINHAS).should('have.length',7)
+        cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should('exist')
+
     })
 
     it('Excluir conta', () => {
