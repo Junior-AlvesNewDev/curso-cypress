c9a381f258dd3c54d52c94f0056e33866fbeaf1b
Junior-AlvesNewDev <onoffjunior@gmail.com>
2025-12-19 22:02:26 -0300

    consulta de saldo, alteração e verificacao de valor, cenario completo

--- cypress/cypress/e2e/barriga/functional.cy.js
+++ cypress/cypress/e2e/barriga/functional.cy.js
@@ -18,11 +18,10 @@ describe('Teste funcional', () => {
     })
 
     it('Alterar conta', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
+        cy.acessarMenuConta()
 
-        cy.get(loc.CONTAS.NOME).type('Conta para alterar')
+        cy.xpath(loc.CONTAS.FN_XP_BTN_ALTERAR('Conta para alterar')).click()
+        cy.get(loc.CONTAS.NOME)
             .clear()
             .type('Conta alterada')
 
@@ -40,15 +39,39 @@ describe('Teste funcional', () => {
     it('Salvando Movimentos', () => {
         cy.get(loc.MENU.MOVIMENTACAO).click();
 
-        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('desc')
         cy.get(loc.MOVIMENTACAO.VALOR).type('123')
         cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta alterada')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
 
         cy.get(loc.EXTRATO.LINHAS).should('have.length',7)
-        cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should('exist')
+        cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO('desc', '123')).should('exist')
 
+    })
+
+    it('Consultar saldo', () => {
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
+
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.xpath(loc.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
+        //cy.wait(1000)
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
+        cy.get(loc.MOVIMENTACAO.STATUS).click()
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        cy.get(loc.MENU.HOME).click()
+        cy.xpath(loc.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
+    })
+
+    it('Remover movimento', () => {
+        cy.get(loc.MENU.EXTRATO).click()
+        cy.xpath(loc.EXTRATO.FN_XP_REMOVER_ELEMENTO('desc')).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
     })
 
     it('Excluir conta', () => {
--- cypress/cypress/support/locators.js
+++ cypress/cypress/support/locators.js
@@ -5,26 +5,36 @@ const locators = {
         BTN_LOGIN: '.btn'
     },
     MENU: {
+        HOME: '[data-test=menu-home]',
         SETTINGS: '[data-test=menu-settings]',
         CONTAS:'[href="/contas"]',
         RESET: '[href="/reset"]',
-        MOVIMENTACAO: '[data-test=menu-movimentacao]'
+        MOVIMENTACAO: '[data-test=menu-movimentacao]',
+        EXTRATO: '[data-test=menu-extrato]'
     },
     CONTAS: {
         NOME: '[data-test=nome]',
     BTN_SALVAR: '.btn',
     XP_BTN_ALTERAR: "//table//td[contains(., 'Conta para alterar')]/.//i[@class='far fa-edit']",
-    XP_BTN_EXCLUIR: "//table//td[contains(., 'Internet webs')]/.//i[@class='far fa-trash-alt']"
+    XP_BTN_EXCLUIR: "//table//td[contains(., 'Internet webs')]/.//i[@class='far fa-trash-alt']",
+    FN_XP_BTN_ALTERAR: conta => `//span[contains(., '${conta}')]/../../..//i[@class='far fa-edit']`
     },
     MOVIMENTACAO:{
         DESCRICAO: '[data-test=descricao]',
         VALOR: '[data-test=valor]',
         INTERESSADO: '[data-test=envolvido]',
+        CONTA: '[data-test=conta]',
+        STATUS: '[data-test=status]',
         BTN_SALVAR: '.btn-primary'
     },
     EXTRATO:{
         LINHAS: '.list-group > li',
-        XP_BUSCA_ELEMENTO:"//span[contains(., 'Desc')]/following-sibling::small[contains(., '123')]"
+        XP_BUSCA_ELEMENTO: (desc, valor) => `//span[contains(., '${desc}')]/following-sibling::small[contains(., '${valor}')]`,
+        FN_XP_REMOVER_ELEMENTO: desc => `//span[contains(., '${desc}')]/../../..//i[@class='far fa-trash-alt']`,
+        FN_XP_ALTERAR_ELEMENTO: conta => `//span[contains(., '${conta}')]/../../..//i[@class='fas fa-edit']`
+    },
+    SALDO:{
+        FN_XP_SALDO_CONTA: nome => `//td[contains(., '${nome}')]/../td[2]`
     },
     MESSAGE:'.toast-message'
 }
